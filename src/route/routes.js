a817caa336d072f9e141d89c9c5b6d31031db8a3
nnsahu02 <nnsahu2022@gmail.com>
2022-12-19 16:41:08 +0530

    changes

--- src/route/routes.js
+++ src/route/routes.js
@@ -8,6 +8,8 @@ router.post('/register',userController.registerUser)
 
 router.post('/awsLink',awsController.createLink )
 
+router.post('/login', userController.login)
+
 router.all('/*', function(req, res){
     res.status(400).send({status: false, message: 'Path not found'})
 })
