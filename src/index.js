2f84eada006d6b4c38c7c3292c81e4f8f70b4c3a
rjROhIT1312 <joshirohit44@gmail.com>
2022-12-19 14:15:04 +0530

    start

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const route = require('./route/route')
 
 app.use(express.json())
 
-mongoose.connect('mongodb+srv://Aman_Mohadikar:V5FW1Y8X6b2pIiud@cluster0.gdww84s.mongodb.net/project-4', {useNewUrlParser: true}, mongoose.set('strictQuery', true))
+mongoose.connect('', {useNewUrlParser: true}, mongoose.set('strictQuery', true))
 .then(() => { console.log('MongoDB is connected')})
 .catch((error) => {console.log(error)})
 
