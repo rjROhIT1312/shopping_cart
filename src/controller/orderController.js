e443c39b298f177855e00ac72fec825a9b425f82
mansi012 <mansigupta18816@gmail.com>
2022-12-26 13:30:28 +0530

    done

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -74,8 +74,36 @@ const createOrder = async function (req, res) {
 //UPDATE ORDER
 const updateOrder = async function (req, res) {
     try {
-        
+        const userId = req.params.userId
+        const bodyData = req.body
+        const { orderId, status } = bodyData
+        if (!orderId) {
+            return res.status(400).send({ status: false, message: "orderId should be present" })
+        }
+        if (!status) {
+            return res.status(400).send({ status: false, message: "status should be present" })
+        }
+        let arr = ["completed", "cancled"]
+        if (!arr.includes(status)) {
+            return res.status(400).send({ status: false, message: "status should follow only this enum[completed, cancled] " })
+        }
+        if (!isIdValid(orderId)) {
+            return res.status(400).send({ status: false, message: "invalid orderId" })
+        }
+        let orderData = await orderModel.findOne({ _id: orderId, userId: userId })
+        if (!orderData) {
+            return res.status(404).send({ status: false, message: "No order found with this userId and orderId." })
+        }
+        if (orderData.cancellable == false) {
+            return res.status(400).send({ status: false, message: "can not update order status,cancellable key is false" })
 
+        }
+        if (orderData.status == status) {
+            return res.status(400).send({ status: false, message: " status is already present,please enter another one" })
+
+        }
+        const updateOrder = await orderModel.findByIdAndUpdate({ _id: orderId }, { $set: { status: status } }, { new: true })
+        return res.status(200).send({ status: true, message: "sucess", data: updateOrder })
     }
     catch (error) {
         return res.status(500).send({ status: false, message: error.message })
